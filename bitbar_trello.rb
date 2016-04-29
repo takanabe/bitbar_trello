f8097f9260e842e06e81cf8fb7192ea3c3be5844
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2016-04-29 09:19:17 +0900

    Extact card names from list

--- bitbar_trello.rb
+++ bitbar_trello.rb
@@ -2,9 +2,21 @@ require 'trello'
 require 'denv'
 require 'pp'
 
+def fetch_doing_lists
+end
+
 
 def fetch_list_name(list)
-  puts list.attributes[:name]
+  list.attributes[:name]
+end
+
+def fetch_cards_from_list(list)
+  list.cards
+end
+
+
+def extract_card_name(card)
+  card.attributes[:name]
 end
 
 def main
@@ -15,11 +27,13 @@ def main
     config.oauth_token = ENV['TRELLO_OAUTH_TOKEN']
   end
 
-  # card = Trello.client.find(:board, 'l9WkaZbY')
-  # pp card.cards
+  list = Trello.client.find(:list, ENV['LIST_ID'])
+  puts fetch_list_name(list)
+  cards = fetch_cards_from_list(list)
 
-  list = Trello.client.find(:list, '566cb8d16ce0bf9fa6d0d434')
-  fetch_list_name(list)
+  cards.each do |card|
+    puts extract_card_name(card)
+  end
 end
 
 
