b99aa2ff9760979960d1179ae05c2334d26a3708
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2016-04-29 11:15:11 +0900

    Load Trello config and envfile

--- bitbar_trello.rb
+++ bitbar_trello.rb
@@ -4,6 +4,24 @@ require 'trello'
 require 'denv'
 require 'pp'
 
+
+def load_dotenv_file(dotenv_file)
+  denv_file_path = if File.symlink?(__FILE__)
+                     File.expand_path('..', File.realpath(__FILE__)) + "/#{dotenv_file}"
+                   else
+                     raise "Pleae execute setup script"
+                   end
+  Denv.load(denv_file_path)
+end
+
+def set_trello_config
+  Trello.configure do |config|
+    config.consumer_key = ENV['TRELLO_CONSUMER_KEY']
+    config.consumer_secret = ENV['TRELLO_CONSUMER_SECRET']
+    config.oauth_token = ENV['TRELLO_OAUTH_TOKEN']
+  end
+end
+
 def fetch_doing_lists
 end
 
@@ -22,21 +40,17 @@ def extract_card_name(card)
 end
 
 def main
-  Denv.load(File.expand_path(File.dirname(__FILE__) + '/.env'))
-  Trello.configure do |config|
-    config.consumer_key = ENV['TRELLO_CONSUMER_KEY']
-    config.consumer_secret = ENV['TRELLO_CONSUMER_SECRET']
-    config.oauth_token = ENV['TRELLO_OAUTH_TOKEN']
-  end
+  dotenv_filename= '.env'
+  load_dotenv_file(dotenv_filename)
+  set_trello_config
 
   list = Trello.client.find(:list, ENV['LIST_ID'])
-  puts fetch_list_name(list)
+  puts 'List: ' + fetch_list_name(list)
   cards = fetch_cards_from_list(list)
 
   cards.each do |card|
-    puts extract_card_name(card)
+    puts 'Card: ' +  extract_card_name(card)
   end
 end
-
 
 main
